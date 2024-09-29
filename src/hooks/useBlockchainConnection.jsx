4a4f7789c16ba54c04559a51642c347de7ab19af
olawalesegun <olawalesegun0@gmail.com>
2024-09-29 19:39:32 +0300

    added cleaning up of events

--- src/hooks/useBlockchainConnection.jsx
+++ src/hooks/useBlockchainConnection.jsx
@@ -1,4 +1,5 @@
 import {useCallback, useState} from 'react';
+// I actually need this to confirm thatMetamask is injected into my Browser
 import detectEthereumProvider from '@metamask/detect-provider';
 import {ethers} from 'ethers';
 
@@ -11,13 +12,19 @@ export function useBlockchainConnection() {
   const [accountConnected, setAccountConnected] = useState("");
 
   const connectWithInjectedProvider = useCallback(async () => {
- 
+    let createdProviderInstance;
+
     const detectedETHProvider = await detectEthereumProvider();
+
     if(detectedETHProvider != null && detectedETHProvider != undefined) {
       console.log(detectedETHProvider)
-      const createdProviderInstance = new ethers.BrowserProvider(detectedETHProvider);
-      setBlockChProvider(createdProviderInstance);
-
+      createdProviderInstance = new ethers.BrowserProvider(detectedETHProvider);
+      if(createdProviderInstance){
+        setBlockChProvider(createdProviderInstance);
+      }else{
+        console.log("there was some error with BrowserProvider");
+      }
+      
       try{
         const userAccounts = await detectedETHProvider.request({ method: 'eth_requestAccounts' });
         if (userAccounts.length > 0) {
@@ -30,29 +37,48 @@ export function useBlockchainConnection() {
       }
 
       const ntwkToBeConnected = await createdProviderInstance.getNetwork();
-      setCurrentNetworkActive(ntwkToBeConnected.name);
+      console.log("The network connected is", ntwkToBeConnected);
+      if(ntwkToBeConnected){
+        setCurrentNetworkActive(ntwkToBeConnected.name);
+      } else{
+        console.log("There was an error in getting the network");
+      }
 
-      detectedETHProvider.on('accountsChanged', (newAcct) => {
-        if(newAcct.length > 0) {
-          setAccountConnected(newAcct[0]);
-        }
-      });
+      detectedETHProvider.on('accountsChanged', handleChangeOfAccount);
 
-      detectedETHProvider.on('chainChanged', async() => {
-        const stateOfNetwork = await createdProviderInstance.getNetwork();
-        setCurrentNetworkActive(stateOfNetwork.name);
-
-      });
+      detectedETHProvider.on('chainChanged', handleChangeOfChain);
     } else {
       console.error("You don't have Metamask");
     }
-  
+    async function handleChangeOfChain() {
+      const stateOfNetwork = await createdProviderInstance.getNetwork();
+      setCurrentNetworkActive(stateOfNetwork.name);
+    }
+
+    function handleChangeOfAccount(newAcct) {
+      if(newAcct.length > 0) {
+        setAccountConnected(newAcct[0]);
+      }
+    }
+
+    return () => {
+      if (detectedETHProvider) {
+        detectedETHProvider.removeListener('accountsChanged', handleChangeOfAccount);
+        detectedETHProvider.removeListener('chainChanged', handleChangeOfChain);
+      }
+    };
   }, []);
 
+  
   const retrieveBal = useCallback(async (address) => {
     if (blockChProvider) {
-      const balFromAccount = await blockChProvider.getBalance(address);
-      setAmountLeftInWallet(ethers.utils.formatEther(balFromAccount));
+      try{
+        const balFromAccount = await blockChProvider.getBalance(address);
+        setAmountLeftInWallet(ethers.utils.formatEther(balFromAccount));  
+      } catch(err){
+        console.error('There was a problem retrieving balance', err);
+      }
+      
     }
   }, [blockChProvider]);
 
